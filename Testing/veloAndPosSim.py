bc17bd93aa2f669b8d286fb777c601a414acf53b
Crystal-Lightning <116341437+Crystal-Lightning@users.noreply.github.com>
2026-09-09 12:27:51 +0530

    minor changes

--- Testing/veloAndPosSim.py
+++ Testing/veloAndPosSim.py
@@ -16,6 +16,7 @@ p_main.start() #threading the process so we can run two functions simaltaneously
 
 #you can remove the part below its just for testing if the positions are running correctly
 #someone please do a matplot for this code
+#t
 while True: 
     print(atoms_p)
     time.sleep(1)
