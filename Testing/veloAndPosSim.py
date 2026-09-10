52dbc6c9836fde6ad6eeae8ccd4d005b614e45aa
Crystal-Lightning <116341437+Crystal-Lightning@users.noreply.github.com>
2026-09-10 12:39:20 +0530

    Generalising the number of atoms (so they can be modified if we want) and linking the parallel simulations with the main daemon

--- Testing/veloAndPosSim.py
+++ Testing/veloAndPosSim.py
@@ -1,9 +1,11 @@
 import threading as thr #so we can run the simulation process as a thread
 import numpy as np
 import time as time
+n = 12 # generalsing the atom count
+distance_matrix=np.zeros(shape=(n,n))
 #treating everthing as m/s
-atoms_v = np.random.uniform(low=-10, high=10, size=(12,3)) #the velocity array, we will modify this for simulating things
-atoms_p = np.zeros(shape=(12,3))
+atoms_v = np.random.uniform(low=-10, high=10, size=(n,3)) #the velocity array, we will modify this for simulating things
+atoms_p = np.zeros(shape=(n,3))
 
 def simulation():
     global atoms_p, atoms_v
@@ -14,12 +16,12 @@ def simulation():
 def distance_sim():
     while True:
         global atoms_p, atoms_v, distance_matrix
-        distance_matrix = (np.sum(((atoms_p.reshape(3,1,12)-atoms_p.reshape(3,12,1))**2),axis=0))**(1/2) 
+        distance_matrix = (np.sum(((atoms_p.reshape(3,1,n)-atoms_p.reshape(3,n,1))**2),axis=0))**(1/2) 
         time.sleep(0.01)
         #like a matrix which contains distances of all 12 atoms with each other pairwise
 
-p_main = thr.Thread(target=simulation)
-p_distance = thr.Thread(target=distance_sim)
+p_main = thr.Thread(target=simulation, daemon=True)
+p_distance = thr.Thread(target=distance_sim, daemon=True)
 p_main.start() #threading the process so we can run two functions simaltaneously
 p_distance.start()
     
